4b35c90863c4bbdb8d4ab9a2209a2f3f8ef244b7
pulkitsinha004 <133797832+sinha004@users.noreply.github.com>
2025-04-17 10:22:50 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,13 @@ import {
   verticalListSortingStrategy,
 } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
+import LinkComponent from "./components/LinkComponent";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const SortableItem = ({ entry, onDelete, index }) => {
   const { attributes, listeners, setNodeRef, transform, transition } =
@@ -93,16 +100,11 @@ const SortableItemDropdown = ({
       {showDropdown && (
         <ul className="absolute bg-white mt-3 shadow-md rounded-md z-10 min-w-[150px]">
           {entries.slice(5).map((entry, index) => (
-            <li key={index + 5} className="hover:bg-gray-100">
-              <a
-                href={entry.url}
-                className="block px-4 py-2 hover:text-blue-600"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {entry.name}
-              </a>
-            </li>
+            <LinkComponent
+              index={index}
+              entry={entry}
+              onDelete={onDelete}
+            ></LinkComponent>
           ))}
         </ul>
       )}
@@ -110,7 +112,7 @@ const SortableItemDropdown = ({
   );
 };
 
-function App() {
+const App = () => {
   const [showForm, setShowForm] = useState(false);
   const [name, setName] = useState("");
   const [url, setText] = useState("");
@@ -120,6 +122,64 @@ function App() {
     return saved ? JSON.parse(saved) : [];
   });
   const [items, setItems] = useState([]);
+
+  useGSAP(() => {
+    const timeline = gsap.timeline();
+
+    timeline.from("nav", {
+      y: -20,
+      opacity: 0,
+      duration: 2,
+      delay: 0.5,
+    });
+    timeline.from("nav div", {
+      y: -20,
+      opacity: 0,
+      duration: 1,
+      delay: 0.2,
+    });
+    timeline.from("nav ul", {
+      y: -20,
+      opacity: 0,
+      duration: 1,
+      stagger: 0.5,
+    });
+
+    timeline.from("section h1", {
+      y: 50,
+      opacity: 0,
+      duration: 1,
+    });
+
+    timeline.from("section h2", {
+      y: 50,
+      opacity: 0,
+      duration: 1,
+    });
+  });
+
+  useGSAP(() => {
+    gsap.to(".logo", {
+      rotate: 360,
+      duration: 0.5,
+      repeat: -1,
+      delay: 5,
+      repeatDelay: 3, // 3s delay + 1s duration = 4s total cycle
+      ease: "none",
+    });
+  }, []);
+
+  useGSAP(() => {
+    const timeline = gsap.timeline();
+
+    if (showForm) {
+      timeline.from("form", {
+        y: -30,
+        opacity: 0,
+        duration: 0.5,
+      });
+    }
+  }, [showForm]);
 
   useEffect(() => {
     localStorage.setItem("entries", JSON.stringify(entries));
@@ -169,103 +229,114 @@ function App() {
   };
 
   return (
-    <div className="bg-amber-50 min-h-screen">
-      <nav className="bg-white shadow-md px-6 py-4 flex justify-between items-center">
-        <div className="url-xl font-bold url-blue-600">LOGO</div>
+    <main className="bg-white text-black min-h-screen w-full font">
+      <div className="w-full h-10/12">
+        <nav className="fixed top-0 left-0 right-0 bg-white z-50 px-8 py-6 flex justify-between items-center border-b border-gray-200 shadow-sm">
+          <div className="logo text-xl font-bold text-blue-700">LOGO</div>
 
-        <DndContext
-          sensors={sensors}
-          collisionDetection={closestCenter}
-          onDragEnd={handleDragEnd}
-        >
-          <SortableContext items={items} strategy={verticalListSortingStrategy}>
-            <ul className="flex gap-6 text-gray-700 font-medium">
-              {items.map((item, index) => {
-                const entry = entries.find((entry) => entry.name === item) || {
-                  name: item,
-                  url: "#",
-                };
-
-                return item === "Links" ? (
-                  <SortableItemDropdown
-                    key={item}
-                    id={item}
-                    showDropdown={showDropdown}
-                    setShowDropdown={setShowDropdown}
-                    entries={entries}
-                    onDelete={onDelete}
-                  />
-                ) : (
-                  <SortableItem
-                    key={item}
-                    entry={entry}
-                    index={index}
-                    onDelete={onDelete}
-                  />
-                );
-              })}
-            </ul>
-          </SortableContext>
-        </DndContext>
-
-        <button
-          className="bg-blue-600 url-white px-4 py-2 rounded-lg hover:bg-blue-700"
-          onClick={() => setShowForm(!showForm)}
-        >
-          Add Entry
-        </button>
-      </nav>
-
-      <main className="p-4">
-        <h1 className="url-3xl font-semibold">Hello!</h1>
-        <h2 className="url-3xl font-semibold">You can use <a className="text-red-500" href="https://www.google.com/">https://www.google.com/</a> for the URL as it will not take anything else than a valid URL</h2>
-
-        {showForm && (
-          <form
-            onSubmit={handleSubmit}
-            className="mt-6 bg-white p-6 rounded-lg shadow-md w-full max-w-md"
+          <DndContext
+            sensors={sensors}
+            collisionDetection={closestCenter}
+            onDragEnd={handleDragEnd}
           >
-            <h2 className="url-2xl font-bold mb-4 url-gray-800">
-              Add New Entry
-            </h2>
-
-            <div className="mb-4">
-              <label className="block mb-2 url-sm font-medium url-gray-700">
-                Name
-              </label>
-              <input
-                type="text"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                className="w-full px-3 py-2 border border-gray-300 rounded-md"
-                required
-              />
-            </div>
-
-            <div className="mb-4">
-              <label className="block mb-2 url-sm font-medium url-gray-700">
-                URL
-              </label>
-              <input
-                type="url"
-                value={url}
-                onChange={(e) => setText(e.target.value)}
-                className="w-full px-3 py-2 border border-gray-300 rounded-md"
-                required
-              />
-            </div>
-
-            <button
-              type="submit"
-              className="bg-green-600 url-white px-4 py-2 rounded hover:bg-green-700"
+            <SortableContext
+              items={items}
+              strategy={verticalListSortingStrategy}
             >
-              Submit
-            </button>
-          </form>
-        )}
-      </main>
-    </div>
+              <ul className="flex gap-6 text-black font-medium">
+                {items.map((item, index) => {
+                  const entry = entries.find(
+                    (entry) => entry.name === item
+                  ) || {
+                    name: item,
+                    url: "#",
+                  };
+
+                  return item === "Links" ? (
+                    <SortableItemDropdown
+                      key={item}
+                      id={item}
+                      showDropdown={showDropdown}
+                      setShowDropdown={setShowDropdown}
+                      entries={entries}
+                      onDelete={onDelete}
+                    />
+                  ) : (
+                    <SortableItem
+                      key={item}
+                      entry={entry}
+                      index={index}
+                      onDelete={onDelete}
+                    />
+                  );
+                })}
+              </ul>
+            </SortableContext>
+          </DndContext>
+
+          <button
+            className="bg-blue-600 text-white px-2 py-3 rounded-xl shadow-lg hover:bg-blue-700 text-sm min-w-[150px] cursor-pointer"
+            onClick={() => setShowForm(!showForm)}
+          >
+            Add Entry
+          </button>
+        </nav>
+
+        <section className="pt-28 p-6 flex flex-col justify-center items-center mt-4">
+          <h1 className="text-3xl font-semibold mb-2">Hello!</h1>
+          <h2 className="text-lg mb-4">
+            You can use
+            <a
+              className="text-red-500 underline mx-1.5"
+              href="https://www.google.com/"
+            >
+              https://www.google.com/
+            </a>
+            for the URL as it will not take anything else than a valid URL.
+          </h2>
+          {showForm && (
+            <form
+              onSubmit={handleSubmit}
+              className="mt-6 bg-gray-100 p-6 rounded-lg shadow-md w-full max-w-lg flex flex-col justify-center"
+            >
+              <h2 className=" text-center text-2xl font-bold mb-4">
+                Add New Entry
+              </h2>
+
+              <div className="mb-4">
+                <label className="block mb-2 text-sm font-medium">Name</label>
+                <input
+                  type="text"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+                  required
+                />
+              </div>
+
+              <div className="mb-4">
+                <label className="block mb-2 text-sm font-medium">URL</label>
+                <input
+                  type="url"
+                  value={url}
+                  onChange={(e) => setText(e.target.value)}
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+                  required
+                />
+              </div>
+
+              <button
+                type="submit"
+                className="bg-green-600 text-white px-4 py-2 rounded shadow hover:bg-green-700 transition"
+              >
+                Submit
+              </button>
+            </form>
+          )}
+        </section>
+      </div>
+    </main>
   );
-}
+};
 
 export default App;
